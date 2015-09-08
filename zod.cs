a5c79f807c11c8e06725a7fe17b958e4b5eaea99
trialless <trialless@gmail.com>
2015-09-08 16:20:52 +0300

    Update zod.cs

--- zod.cs
+++ zod.cs
@@ -1,9 +1,1 @@
-using UnityEngine;
-using System.Collections;
-public class zod {
-string url = "http://2tqqt82jmt7v3yb42gsd75a19wd.wpengine.netdna-cdn.com/wp-content/plugins/horoscope-app//horoscope.min.js?ver=0.1.0";
-
-public void prd(){
- Debug.Log("da"); 
-}
-}
+../
